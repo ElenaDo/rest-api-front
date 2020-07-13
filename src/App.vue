afa809903a0eb3de72834d88f90a18f61d39b17f
Elena Raspopova <elenado@Elenas-Air.fritz.box>
2020-07-13 11:21:47 +0200

    basic form added

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  </div>
+  <v-app>
+    <h1 class="text-center">Rest API</h1>
+    <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="3"
+        >
+          <v-text-field
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="3"
+        >
+          <v-text-field
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="3"
+        >
+          <v-text-field
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="12"
+          md="3"
+        >
+        <v-btn color="primary">Create</v-btn>
+        </v-col>
+      </v-row>
+      <v-card>
+        <v-card-title>Name</v-card-title>
+      </v-card>
+    </v-container>
+  </v-form>
+  </v-app>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
